375a257d2f3416e29ad3e56b3ed03ca971702567
Artem Barkalov <artembark@gmail.com>
2025-02-26 05:16:16 +0300

    feat: добавить скоуп с внутренней зависимостью от репозитория

--- lib/features/profile_scope/presentation/screens/profile_scope_screen.dart
+++ lib/features/profile_scope/presentation/screens/profile_scope_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:friflex_starter/app/app_context_ext.dart';
 import 'package:friflex_starter/features/profile_scope/domain/bloc/profile_scope_bloc.dart';
 import 'package:friflex_starter/features/profile_scope/presentation/profile_scope.dart';
 
@@ -11,32 +10,19 @@ class ProfileScopeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ProfileScope(
-      profileRepository: context.di.repositories.profileScopeRepository,
-      child: const _ProfileScopeView(),
-    );
-  }
-}
-
-class _ProfileScopeView extends StatelessWidget {
-  const _ProfileScopeView();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile Scope'),
-      ),
-      body: Center(
-        child: BlocBuilder<ProfileScopeBloc, ProfileScopeState>(
-          bloc: ProfileScope.of(context).profileScopeBloc
-            ..add(const ProfileScopeFetchProfileEvent(id: '1')),
-          builder: (context, state) {
-            return switch (state) {
-              ProfileScopeSuccessState() => Text('Data: ${state.props.first}'),
-              ProfileScopeErrorState() => Text('Error: ${state.message}'),
-              _ => const CircularProgressIndicator(),
-            };
-          },
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Profile Scope')),
+        body: Center(
+          child: BlocBuilder<ProfileScopeBloc, ProfileScopeState>(
+            bloc: ProfileScope.of(context).profileScopeBloc,
+            builder: (context, state) {
+              return switch (state) {
+                ProfileScopeSuccessState() => Text('Data: ${state.props.first}'),
+                ProfileScopeErrorState() => Text('Error: ${state.message}'),
+                _ => const CircularProgressIndicator(),
+              };
+            },
+          ),
         ),
       ),
     );
